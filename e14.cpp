488164a74598781112a7bbc8ebaffd784043ceab
davillbert <56187467+davillbert@users.noreply.github.com>
2019-12-14 00:43:01 +0300

    Update e14.cpp

--- e14.cpp
+++ e14.cpp
@@ -10,15 +10,19 @@ int main()
     ULL rocks;
     cin >> rocks;
     ULL *winners = (ULL*)malloc(sizeof(ULL)*(rocks));
-
+for(ULL i = 1; i <= rocks; ++i)
+{
+    winners[i] = 0;
+}
     winners[1] = 1;
     winners[2] = 0;
     for( ULL i = 3; i <= rocks; ++i)
     {
         for(int j = i - int(sqrt(i)); j <= i - 1; ++j)
-            if(winners[j] == 0){ winners[i] == 1; }
+            if(winners[j] == 0){ winners[i] = 1; }
     }
 
     if(winners[rocks]) cout << "First" << endl;
     else cout<< "Second" << endl;
+    free(winners);
 }
