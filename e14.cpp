7c687746104e20e20c9fb5af7b0b140c7450d1a6
davillbert <56187467+davillbert@users.noreply.github.com>
2019-12-14 01:16:10 +0300

    Update e14.cpp

--- e14.cpp
+++ e14.cpp
@@ -1,6 +1,4 @@
-#include <string.h>
 #include <iostream>
-#include <vector>
 #include <math.h>
 using namespace std;
 using ULL = unsigned long long;
@@ -9,21 +7,25 @@ int main()
 {
     int rocks;
     cin >> rocks;
-    int *winners = (int*)malloc(sizeof(int)*(rocks + 1));
-for(int i = 1; i <= rocks; ++i)
-{
-    winners[i] = 0;
-}
+    int* winners = (int*)malloc(sizeof(int) * (rocks + 1));
+    for (int i = 1; i <= rocks; ++i)
+    {
+        winners[i] = 0;
+    }
     winners[1] = 1;
     winners[2] = 0;
-    for( int i = 3; i <= rocks; ++i)
+    for (int i = 3; i <= rocks; ++i)
     {
-        for(int j = i - int(sqrt(i)); j <= i - 1; ++j)
-            if(winners[j] == 0){ winners[i] = 1; }
+        for (int j = i - int(sqrt(i)); j <= i - 1; ++j)
+            if (winners[j] == 0)
+            {
+                winners[i] = 1;
+            }
     }
 
-    if(winners[rocks]) cout << "First" << endl;
-    else cout<< "Second" << endl;
+    if (winners[rocks])
+        cout << "First" << endl;
+    else
+        cout << "Second" << endl;
     free(winners);
 }
-
