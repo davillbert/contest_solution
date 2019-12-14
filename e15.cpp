45092b3618c06674b71fd8c38e6a743760dc21cf
davillbert <56187467+davillbert@users.noreply.github.com>
2019-12-15 02:36:35 +0300

    Update e15.cpp

--- e15.cpp
+++ e15.cpp
@@ -5,27 +5,30 @@ using namespace std;
 
 int main()
 {
-    int N, M, MOD;
+    long long N, M, MOD;
     cin >> N >> M >> MOD;
-    int* a = (int*)malloc(sizeof(int) * (N + 1));
-    int* S = (int*)malloc(sizeof(int) * (M));
-    for (int i = N; i >= 0; --i)
+    long long* a = (long long*)malloc(sizeof(long long) * (N));
+    long long* S = (long long*)malloc(sizeof(long long) * (M));
+    for (long long i = N; i >= 0; --i)
     {
         cin >> a[i];
     }
-    for (int j = 0; j < M; ++j)
+    for (long long j = 0; j < M; ++j)
     {
         S[j] = 0;
     }
-    for (int j = 0; j < M; ++j)
+    for (long long j = 0; j < M; ++j)
     {
-        int x;
+        long long x;
         cin >> x;
-        for (int i = 0; i < N; ++i)
+        for (long long i = N; i > 0; --i)
         {
            S[j] =(S[j] * x + a[i]) % MOD;
         }
-        cout << (S[j] * x + a[N]) % MOD << endl;
+        cout << endl;
+        cout << (S[j] * x + a[0]) % MOD ;
     //    S[j] = 0;
     }
+    free(a);
+    free(S);
 }
